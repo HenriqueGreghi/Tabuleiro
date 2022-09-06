7b0fdfbb91a79576772ade05ec925efd19db562c
Henrique Greghi <henriquevsg@gmail.com>
2022-09-06 11:38:13 -0300

    Criando tabuleiro

---
+++ Tabuleiro/Calculo.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tabuleiro
+{
+    class Calculo
+    {
+        private int A { get; set; }
+        private int B { get; set; }
+        private int N { get; set; }
+        public int dimencao { get; set; }
+
+        public void Calcular()
+        {
+            N = dimencao * dimencao;
+            if (N % 2 == 0)
+            {
+                A = N / 2;
+                B = A;
+                
+            }
+            else
+            {
+                A = (N + 1) / 2;
+                B = A - 1;
+                
+            }
+        }
+        public void Imprimir()
+        {
+            Console.WriteLine(A + " casas brancas e " + B + " casas pretas.");
+        }
+    }
+}
---
+++ Tabuleiro/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Tabuleiro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculo calc = new Calculo();
+            
+            Console.WriteLine("Entre com a dimenção do tabuleiro: ");
+            calc.dimencao = int.Parse(Console.ReadLine());
+
+            calc.Calcular();
+            calc.Imprimir();
+            
+
+        }
+    }
+}
